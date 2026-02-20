d089174df87ec474bfadd918bfa9418ae2101b37
Sandra Amurao <sandramae421@gmail.com>
2026-02-20 09:55:34 +0000

    restructure main and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { createRoot } from "react-dom/client";
+import FilterBar from "./FilterBar.jsx";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+	const [active, setActive] = useState("All");
+
+	function activateFilterStatus(activeFilter) {
+    setActive(activeFilter);
+		console.log("active", active);
+		console.log(activeFilter);
+	}
+
+	console.log("2active", active);
+	return (
+		<>
+			<h1>Job Application Tracker</h1>
+			<p>
+				Did you apply to a new job posting? Add it here to keep track of it!
+			</p>
+			<FilterBar activateFilterStatus={activateFilterStatus} active={active} />
+		</>
+	);
 }
+
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,8 @@
 import { createRoot } from "react-dom/client";
-import FilterBar from "./FilterBar.jsx";
-import { useState } from "react";
-import "./main.css";
+import App from "./App.jsx";
 
-function Home() {
-	const [active, setActive] = useState("All");
-
-	function activateFilterStatus(activeFilter) {
-    setActive(activeFilter);
-		console.log("active", active);
-		console.log(activeFilter);
-	}
-
-	console.log("2active", active);
-	return (
-		<>
-			<h1>Job Application Tracker</h1>
-			<p>
-				Did you apply to a new job posting? Add it here to keep track of it!
-			</p>
-			<FilterBar activateFilterStatus={activateFilterStatus} active={active} />
-		</>
-	);
+function Main() {
+  return <App />;
 }
 
-createRoot(document.getElementById("root")).render(<Home />);
+createRoot(document.getElementById("root")).render(<Main />);
