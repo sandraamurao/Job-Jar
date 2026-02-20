a4d27ecd2e3b106f886eaa4da656ba16820ad863
Sandra Amurao <sandramae421@gmail.com>
2026-02-20 09:50:05 +0000

    fix in constants

---
+++ src/utils/constants.jsx
@@ -0,0 +1,18 @@
+export const jobStatus = {
+    all: "All",
+    applied: "Applied",
+    interview: "Interview",
+    offer: "Offer",
+    rejected: "Rejected"
+}
+
+export const jobApplicationDetails = {
+    id: 0,
+    company: "",
+    jobTitle: "",
+    status: jobStatus.all,
+    dateApplied: "",
+    interviewDate: "",
+    jobUrl: "",
+    notes: "",
+}
