91358f7cc4e88873d0dd369e3edac83fe38d1943
Sandra Amurao <sandramae421@gmail.com>
2026-02-20 09:43:34 +0000

    set up constants

---
+++ src/constants.jsx
@@ -0,0 +1,18 @@
+const jobStatus = {
+    all: "All",
+    applied: "Applied",
+    interview: "Interview",
+    offer: "Offer",
+    rejected: "Rejected"
+}
+
+const jobApplicationDetails = {
+    id: 0,
+    company: "",
+    jobTitle: "",
+    status: jobStatus.all,
+    dateApplied: "",
+    interviewDate: "",
+    jobUrl: "",
+    notes: "",
+}
