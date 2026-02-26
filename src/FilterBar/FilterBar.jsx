f99ef91a3a2d4ddb69bc8c4da655d4d47a9e4fdf
Sandra Amurao <sandramae421@gmail.com>
2026-02-26 12:32:36 +0000

    adjusted styles for add application btn

--- src/FilterBar/FilterBar.jsx
+++ src/FilterBar/FilterBar.jsx
@@ -7,18 +7,17 @@ function FilterBar({ activateFilterStatus, active, onNewApplication }) {
 		<div className="btn-container">
 			{filters.map((filter) => (
 				<button
-					key={filter} // like a name; just for React internally to track which item is which when the list updates.
+					key={filter}
 					onClick={() => activateFilterStatus(filter)}
 					className={`filter-btn ${active === filter ? "btn-active" : "btn"}`}
 				>
 					{filter}
 				</button>
 			))}
-			<button
-				className="new-application-btn"
-				onClick={onNewApplication}
-			>
-				+ New Application
+			
+			<button className="new-application-btn flex flex-row items-center gap-1" onClick={onNewApplication}>
+				<span className="material-icons-outlined">add</span>
+				New Application
 			</button>
 		</div>
 	);
